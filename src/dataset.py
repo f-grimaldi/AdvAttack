727c23893509424108fba2b79d9eb41c7cd38aeb
Damiano Clementel <damiano.clementel@gmail.com>
2020-06-14 16:06:33 +0200

    Dataset classes refactoring

--- src/dataset.py
+++ src/dataset.py
@@ -1,104 +1,120 @@
-### BASE
-from tqdm import tqdm
-### CLASSIC
-import cv2
-import PIL
-import numpy as np
-import matplotlib.pyplot as plt
-### TORCH
-import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils, datasets
-from torch import nn, optim
+# ### BASE
+# from tqdm import tqdm
+# ### CLASSIC
+# import cv2
 
-### BASE
-from tqdm import tqdm
-### CLASSIC
-import cv2
-import PIL
-import numpy as np
-import matplotlib.pyplot as plt
-### TORCH
-import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils, datasets
-from torch import nn, optim
 
-"""
-Take 1 channel Tensor and return a 3 channel Tensor with same values for each channel
-"""
-class RGB(object):
+# ### TORCH
+# import torch
+# from torch.utils.data import Dataset, DataLoader
+
+# from torch import nn, optim
+
+# Dependencies
+import matplotlib.pyplot as plt
+import numpy as np
+from torchvision import transforms, datasets
+import torch
+import PIL
+
+
+class ToRGB(object):
+    """
+    RGB transformer
+    Takes 1 channel Tensor and return a 3 channel Tensor with same values
+    for each channel
+    """
 
     def __call__(self, tensor):
         return tensor.expand((3, tensor.shape[1], tensor.shape[2]))
 
-"""MNIST dataset"""
-class MNIST(Dataset):
+
+class MNIST(torch.utils.data.Dataset):
+    """MNIST dataset"""
 
     def __init__(self, root='data/mnist', transform='standard', upscaling_dim=(299, 299)):
         """
-        Args:
-        root                 (str)                      The root path of the dataset
-        transform            (torchvision.transforms)    The transfrom to apply. Options are:
-                                                                                'standard':  (ToTensor). Default
-                                                                                 'upscale:   (Resize, ToTensor, RGB)
-        upscaling_dim        (tuple)                    The dimension of the upscaling
+        Args
+        root (str):                         The root path of the dataset
+        transform (torchvision.transforms): Transformation pipeline to apply
+        upscaling_dim (tuple):              The dimension of the upscaling
         """
         self.dim = upscaling_dim
         self.root = root
         self.transform = self.get_transform(transform)
-        self.get_dataset()
+        self.fetch_dataset()
 
-    """
-    Get the Transform
-    """
     def get_transform(self, transform):
+        """
+        Get the chosen transformer
+        """
         if transform == 'standard':
-            return transforms.Compose([transforms.ToTensor()])
+            return transforms.Compose([
+                transforms.ToTensor()
+            ])
 
         elif transform == 'upscale':
-             return transforms.Compose([transforms.Resize(self.dim, PIL.Image.LANCZOS),
-                                        transforms.ToTensor(),
-                                        RGB()])
+            return transforms.Compose([
+                transforms.Resize(self.dim, PIL.Image.LANCZOS),
+                transforms.ToTensor(),
+                ToRGB()
+            ])
+        elif type(transform) == torch.transforms.Compose:
+            return transform
         else:
-            if type(transform) == torch.transforms.Compose:
-                return transform
-            else:
-                raise NotImplementedError
+            raise NotImplementedError('Chosen transformer is not valid')
 
-    """
-    Get dataset
-    """
-    def get_dataset(self):
-        self.train_dataset = datasets.MNIST(root=self.root, train=True, download=True, transform=self.transform)
-        self.test_dataset = datasets.MNIST(root=self.root, train=False, download=True, transform=self.transform)
+    def fetch_dataset(self):
+        """
+        Download MNIST dataset
+        """
+        self.train_dataset = datasets.MNIST(
+            root=self.root,  # Define where dataset must be stored
+            train=True,  # Retrieve training partition
+            download=True,  # Retrieve dataset from remote repo
+            transform=self.transform  # Apply chosen transforms
+        )
+        self.test_dataset = datasets.MNIST(
+            root=self.root,
+            train=False,
+            download=True,
+            transform=self.transform
+        )
 
-    """
-    Get dataloader
-    """
     def get_dataloader(self, batch_size, num_workers):
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.testloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size,
-                                                      shuffle=False, num_workers=num_workers)
-        self.trainloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size,
-                                                       shuffle=True, num_workers=num_workers)
-        return self.trainloader, self.testloader
+        """
+        Get DataLoader instance from Dataset instance
+        """
+        # Get test dataset DataLoader object
+        test_loader = torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=batch_size,
+            shuffle=False, num_workers=num_workers
+        )
+        # Get train dataset DataLOader object
+        train_loader = torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers
+        )
+        # Set training data loader
+        return train_loader, test_loader
 
-
-    """
-    Visualize first n example
-    """
-    def get_info(self):
-        for batch in iter(self.trainloader):
-            example = batch
-            print('\nBatch is a {}'.format(type(batch)))
-            print('The 1st element is a {} with shape {}'.format(type(batch[0]), batch[0].shape))
-            print('The 2nd element is a {} with shape {}'.format(type(batch[1]), batch[1].shape))
-            break
-
-        X, y = example[0][:5], example[1][:5]
-
+    @classmethod
+    def get_info(cls, data_loader):
+        """
+        Given a DataLoader object, return some info
+        """
+        # Get first batch in given DataLoader object
+        batch = next(iter(data_loader))
+        # Print information
+        print('\nBatch is a {}'.format(type(batch)))
+        print('The 1st element is a {} with shape {}'.format(type(batch[0]), batch[0].shape))
+        print('The 2nd element is a {} with shape {}'.format(type(batch[1]), batch[1].shape))
+        # Get first 5 rows of X covariates and y target value
+        X, y = batch[0][:5], batch[1][:5]
+        # Make plot
         fig, ax = plt.subplots(1, 5, figsize=(20, 6))
         for n in range(5):
             img = np.transpose(X[n].numpy(), (1, 2, 0))
@@ -109,80 +125,92 @@ class MNIST(Dataset):
         plt.show()
 
 
-"""CIFAR10 dataset"""
-class CIFAR10(Dataset):
+class CIFAR10(torch.utils.data.Dataset):
+    """CIFAR10 dataset"""
 
-    def __init__(self, root = 'data/cifar10', transform='standard', upscaling_dim=(299, 299)):
+    # Static attributes
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    def __init__(self, root='data/cifar10', transform='standard', upscaling_dim=(299, 299)):
         """
-        Args:
-        root                 (str)                      The root path of the dataset
-        transform            (torchvision.transforms)   The transfrom to apply. Options are:
-                                                                                'standard':  (ToTensor). Default
-                                                                                'upscale:   (Resize, ToTensor)
-        upscaling_dim        (tuple)                    The dimension of the upscaling
+        Args
+        root (str):                         The root path of the dataset
+        transform (torchvision.transforms): Transformation pipeline to apply
+        upscaling_dim (tuple):              The dimension of the upscaling
         """
         self.dim = upscaling_dim
-        self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         self.transform = self.get_transform(transform)
         self.root = root
-        self.get_dataset()
+        self.fetch_dataset()
 
-    """
-    Get the Transform
-    """
     def get_transform(self, transform):
         if transform == 'standard':
-            return transforms.Compose([transforms.ToTensor()])
+            return transforms.Compose([
+                transforms.ToTensor()
+            ])
         if transform == 'upscale':
-            return transforms.Compose([transforms.Resize(self.dim, PIL.Image.LANCZOS),
-                                       transforms.ToTensor()])
+            return transforms.Compose([
+                transforms.Resize(self.dim, PIL.Image.LANCZOS),
+                transforms.ToTensor()
+            ])
+        elif type(transform) == torch.transforms.Compose:
+            return transform
         else:
-            if type(transform) == torch.transforms.Compose:
-                return transform
-            else:
-                raise NotImplementedError
+            raise NotImplementedError('Chosen transformer is not valid')
 
-    """
-    Get dataset
-    """
-    def get_dataset(self):
-        self.train_dataset = datasets.CIFAR10(root=self.root, train=True, download=True, transform=self.transform)
-        self.test_dataset = datasets.CIFAR10(root=self.root, train=False, download=True, transform=self.transform)
+    def fetch_dataset(self):
+        """
+        Download CIFAR10 dataset
+        """
+        self.train_dataset = datasets.CIFAR10(
+            root=self.root, train=True, download=True, transform=self.transform
+        )
+        self.test_dataset = datasets.CIFAR10(
+            root=self.root, train=False, download=True, transform=self.transform
+        )
 
-    """
-    Get dataloader
-    """
     def get_dataloader(self, batch_size, num_workers):
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.testloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size,
-                                                      shuffle=False, num_workers=num_workers)
-        self.trainloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size,
-                                                       shuffle=True, num_workers=num_workers)
-        return self.trainloader, self.testloader
+        # self.batch_size = batch_size
+        # self.num_workers = num_workers
+        test_loader = torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers
+        )
+        train_loader = torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers
+        )
+        return test_loader, train_loader
 
-
-    """
-    Visualize first n example
-    """
-    def get_info(self):
-        for batch in iter(self.trainloader):
-            example = batch
-            print('\nBatch is a {}'.format(type(batch)))
-            print('The 1st element is a {} with shape {}'.format(type(batch[0]), batch[0].shape))
-            print('The 2nd element is a {} with shape {}'.format(type(batch[1]), batch[1].shape))
-            break
-
-        batch = example[0][:5]
+    @classmethod
+    def get_info(cls, data_loader):
+        # Retrieve first batch
+        batch = next(iter(data_loader))
+        # Print information
+        print('\nBatch is a {}'.format(type(batch)))
+        print('The 1st element is a {} with shape {}'.format(type(batch[0]), batch[0].shape))
+        print('The 2nd element is a {} with shape {}'.format(type(batch[1]), batch[1].shape))
+        # Get first 5 rows of X covariates and y target value
+        X, y = batch[0][:5], batch[1][:5]
+        # Make plot
         fig, ax = plt.subplots(1, 5, figsize=(20, 6))
         for n in range(5):
-            img = np.transpose(batch[n].numpy(), (1, 2, 0))
+            img = np.transpose(X[n].numpy(), (1, 2, 0))
             ax[n].imshow(img)
-            ax[n].set_title(self.classes[example[1][n]])
+            ax[n].set_title(cls.classes[y[n]])
         plt.show()
 
 
+# Unit testing
 if __name__ == '__main__':
-    DataLoader = CIFAR10()
-    train, test = DataLoader.get_dataloader(32, 4)
-    DataLoader.get_info()
+
+    # Generate new CIFAR10 instance
+    cifar10 = CIFAR10()
+    # Get train and test data
+    train, test = cifar10.get_dataloader(32, 4)
+    # Get info for train data
+    CIFAR10.get_info(train)
